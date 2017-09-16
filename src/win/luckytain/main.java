90df29caeb131e373ba020f659bd44d7068f81ff
BuildTools <unconfigured@null.spigotmc.org>
2017-09-16 21:35:21 +0800

    a lot of bug

---
+++ src/win/luckytain/main.java
@@ -0,0 +1,43 @@
+package win.luckytain;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
+import net.milkbowl.vault.economy.Economy;
+import java.util.Collection;
+
+
+public class main extends JavaPlugin implements Listener {
+    private Economy economy;
+    public void setupEconomy()
+    {
+        RegisteredServiceProvider<Economy> economyProvider =  getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+        if (economyProvider != null) {
+            economy = economyProvider.getProvider();
+        }
+    }
+    @Override
+    public void onEnable() {
+        getLogger().info("BanPoorMan插件开启");
+        Bukkit.getPluginManager().registerEvents(this, this);
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                Collection<? extends Player> pl = Bukkit.getOnlinePlayers();
+                for (Player p : pl){
+                    if (economy.getBalance(p) <= 0){
+                        p.sendMessage("no money");
+                    }
+                }
+            }
+        }.runTaskTimerAsynchronously(this,0,20);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("BanPoorMan插件关闭");
+    }
+}
